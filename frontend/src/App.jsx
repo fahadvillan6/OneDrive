eccebf5c67b46b5ba55e58c6b0b19988e69c0a13
unknown <fahadvillan6@gmail.com>
2023-02-01 18:02:50 +0530

    Deadline-Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css';
 
 function App() {
-  var odOptions = {
+  const odOptions = {
     clientId: '9e63e70f-5443-4649-8d4b-b7df4d958bac',
     action: 'query',
     multiSelect: true,
-    openInNewWindow: false,
     advanced: {
-      advanced: {
-        redirectUri: 'http://localhost:5173',
-      },
+      redirectUri: 'http://localhost:5173',
     },
 
     success: function (files) {
@@ -27,8 +24,37 @@ function App() {
   const oneDriveOpen = () => {
     window.OneDrive.open(odOptions);
   };
+  const launchSaveToOneDrive = () => {
+    let odOptions = {
+      clientId: '9e63e70f-5443-4649-8d4b-b7df4d958bac',
+      action: 'save',
+      sourceInputElementId: 'fileUploadControl',
+      sourceUri: '',
+      fileName: 'Screenshot.png',
+      openInNewWindow: false,
+      advanced: {
+        redirectUri: 'http://localhost:5173',
+      },
+      success: function (files) {
+        console.log('ok');
+      },
+      progress: function (percent) {
+        console.log(percent);
+      },
+      cancel: function () {
+        /* cancel handler */
+      },
+      error: function (error) {
+        console.error(error, 'err');
+      },
+    };
+    OneDrive.save(odOptions);
+  };
+
   return (
     <div className='App'>
+      <input id='fileUploadControl' name='fileUploadControl' type='file' />
+      <button onClick={launchSaveToOneDrive}>Save to OneDrive</button>
       <button onClick={oneDriveOpen}>open Picker</button>
     </div>
   );
